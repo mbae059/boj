f6bedec45a5e605897acf6e38d497f942d287ce8
mbae059 <mbae059@gmail.com>
2023-07-13 05:26:34 +0900

    [Platinum IV] Title: 그래프의 싱크, Time: 16 ms, Memory: 3156 KB -BaekjoonHub

--- 백준/Platinum/6543. 그래프의 싱크/그래프의 싱크.cc
+++ 백준/Platinum/6543. 그래프의 싱크/그래프의 싱크.cc
@@ -2,299 +2,459 @@
 #define endl '\n'
 #define INF 987654321
 #define p_q priority_queue
-#define pb push_back
-#define FOR(i, n) for(int i=0;i<(n);++i)
+#define pbk push_back
+#define rep(i, a, b) for (int i=a; i<=b; i++) 
+#define all(v) (v).begin(), (v).end()
+
 using namespace std;
-typedef pair<int, int> pii;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<int> vi;
-typedef vector<ull> vull;
-typedef vector<vi> vii;
-typedef vector<pii> vpii;
-typedef vector<ll> vll;
-int T, N, K, M;
-//ofstream out("temp.txt");
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vull = vector<ull>;
+using vvi = vector<vi>;
+using vpii = vector<pii> ;
+using vll = vector<ll>;
+using mii = map<int, int>;
+using si = set<int>;
+using qi = queue<int>;
+using qpii = queue<pii>;
+using tiii = tuple<int, int, int> ; //get<0>(t);
+using tlll = tuple<ll, ll, ll> ; //get<0>(t);
+using vtiii = vector<tiii>;
+using vtlll = vector<tlll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+using spii = set<pii>;
+using qtiii = queue<tiii>;
+int A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+int dy[8] = {1,-1,0,0,1,1,-1,-1};
+int dx[8] = {0,0,1,-1,1,-1,1,-1};
+//for tenary search
+//mid1 = (2*l + r)/3;
+//mid2 = (l + 2*r)/3;
 
-void print(int a, int b) {
-	cout << a << " " << b << endl;
+//for finding the intersection of Line(x1,y1,x2,y2) and Line(x3,y3,x4,y4)
+//do not solve with tenary search
+// Px= (x1*y2 - y1*x2)*(x3-x4) - (x1-x2)*(x3*y4 - y3*x4)
+// Py= (x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4)
+
+//int to string : to_string
+//string to int : stoi
+
+//use setw(3) to get nice format for printing out 2-d array
+//ex) cout << setw(3) << "a" << endl;
+
+//to make a sorted vector's element unique, you should do v.erase(unique(v.begin(), v.end()), v.end())
+
+// unordered_map<char,int> dx = {{'D',0},{'L',-1},{'R',1},{'U',0}};
+// unordered_map<char,int> dy = {{'D',1},{'L',0},{'R',0},{'U',-1}}
+
+//diagonal counting. l[y+x], r[y-x+N]
+
+//supports negative modular operation
+ll modular(ll num, ll mod) {
+    num%=mod;
+    return num < 0 ? num+mod : num;
 }
-
 void print(pii a) {
-	cout << a.first << " " << a.second << endl;
+    cout << a.first << " " << a.second << endl;
 }
 
-void print(int a) {
-	cout << a << endl;
+template <typename T>
+void print(const T& a) {
+    cout << a << endl;
 }
 
-void print(vi& v) {
-	for (auto i : v) {
-		cout << i << " ";
-	}
-	cout << endl;
+void print(const vector<pii>& v) {
+    for(auto p : v) {
+        print(p);
+    }
+    cout << endl;
 }
-/* FOR COUNTING INVERSION, MERGE SORT VERSION
-void merge(vector<int>& arr, int l, int mid, int r, int& ic) {
-	vector<int> left, right;
-	for (int i = l; i <= mid; i++) left.pb(arr[i]);
-	for (int i = mid + 1; i <= r; i++) right.pb(arr[i]);
-
-	int i = 0; int j = 0;
-	int n1 = left.size();
-	int n2 = right.size();
-
-	int k = l;
-	while (i < n1 && j < n2) {
-		if (left[i] <= right[j]) {
-			arr[k++] = (left[i++]);
+template <typename T>
+void print(const vector<T>& v) {
+    for(auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+bool inRange(int y, int x) {
+    return 1<=y && y<=N && 1<=x && x<=M;
+}
+bool inRangeN(int y, int x) {
+    return 1<=y && y<=N && 1<=x && x<=N;
+}
+/*
+vector<int> combination;
+bool visited[1005];
+void dfs(int idx, int cnt) { //implement with dfs(1, 0). N and K must be global variable
+	if(cnt==K) {
+		for(auto i : combination) {
+			cout << i << " ";
 		}
-		else {
-			arr[k++] = (right[j++]);
-			ic += (n1 - i);
-		}
+		cout << endl;
+		return;
 	}
-	while (i < n1) {
-		arr[k++] = (left[i++]);
+	
+	for(int i=idx;i<=N;i++) { //idx, N을 잘볼것
+		if(visited[i]==1) continue;
+		
+		visited[i]=1;
+		combination.push_back(i);
+		dfs(i+1, cnt+1); //be careful with i+1, cnt+1
+		visited[i]=0;
+		combination.pop_back();
 	}
-	while (j < n2) {
-		arr[k++] = (right[j++]);
-	}
-}
-
-void mergesort(vector<int>& arr, int l, int r, int& ic) {
-	if (l >= r) return;
-	int mid = (l + r) / 2;
-	mergesort(arr, l, mid, ic);
-	mergesort(arr, mid + 1, r, ic);
-
-	merge(arr, l, mid, r, ic);
-}
-
-int find_inversion_count(vector<int>& arr) {
-	int n = arr.size();
-	int ic = 0;
-	mergesort(arr, 0, n - 1, ic);
-	return ic;
-}
-
-
-
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-
-
-/* THIS IS SEGMENT TREE AND LAZY PROPOGATION
-int segment(int node, int nodeLeft, int nodeRight) { // use when s, v is available and segment tree is about sum
-	if (nodeLeft == nodeRight) {
-		return s[node] = v[nodeLeft];
-	}
-
-	int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-
-	return s[node] = segment(node * 2, nodeLeft, mid) + segment(node * 2 + 1, mid + 1, nodeRight);
-}
-
-int update(int node, int idx, int nodeLeft, int nodeRight, int dif) { //Before this you have to do v[idx]+=dif;
-	if (idx < nodeLeft || nodeRight < idx) return 0;
-
-	if (nodeLeft == nodeRight) return s[node] += dif;
-
-	int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-
-	return s[node] = update(node * 2, idx, nodeLeft, mid, dif) + update(node * 2 + 1, idx, mid + 1, nodeRight, dif);
-}
-
-int query(int node, int l, int r, int nodeLeft, int nodeRight) {
-	if (nodeRight < l || r < nodeLeft) return 0;
-
-	if (l <= nodeLeft && nodeRight <= r) return s[node];
-	int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-	return query(node * 2, l, r, nodeLeft, mid) + query(node * 2 + 1, l, r, mid + 1, nodeRight);
-}
-
-void propogation(int node, int l, int r) {
-	if (lazy[node]) {
-		s[node] += (r - l + 1) * lazy[node];
-		if (l != r) {
-			lazy[node * 2] += lazy[node];
-			lazy[node * 2 + 1] += lazy[node];
-		}
-		lazy[node] = 0;
-	}
-}
-
-void update(int node, int l, int r, int nodeLeft, int nodeRight, int dif) { //This is for lazy propogation
-	propogation(node, nodeLeft, nodeRight);
-	if (nodeRight < l || r < nodeLeft) return;
-
-	if (l <= nodeLeft && nodeRight <= r) {
-		s[node] += (r - l + 1) * dif;
-		if (nodeLeft != nodeRight) {
-			lazy[node * 2] += dif;
-			lazy[node * 2 + 1] += dif;
-		}
-		lazy[node] = 0;
-	}
-
-	int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-
-	update(node * 2, l, r, nodeLeft, mid, dif);
-	update(node * 2 + 1, l, r, mid + 1, nodeRight, dif);
-
-	s[node] = s[node * 2] + s[node * 2 + 1];
-}
-
-ll query(int node, int l, int r, int nodeLeft, int nodeRight) { //s should be vll
-	propogation(node, nodeLeft, nodeRight);
-	if (nodeRight < l || r < nodeLeft) return;
-
-	if (l <= nodeLeft && nodeRight <= r) {
-		return s[node];
-	}
-
-	int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-
-	return query(node * 2, l, r, nodeLeft, mid)+query(node * 2+1, l, r, mid+1, nodeRight);
-
 }
 */
-
+/* Basic Info
+A-Z is 26 char
+<regex> header file is used for find patterns
+*/
 
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 
 
+// This is segment tree
+/*
+#define MAX 100001
+ll a[MAX], s[4*MAX];
 
-/* THIS IS FENWICK_TREE
-void Update(int Idx, int Value) { //For Making Fenwick Tree, for(int i=1~N) Update(i, Arr[i]);
-	while (Idx < Fenwick_Tree.size()) {
-		Fenwick_Tree[Idx] = Fenwick_Tree[idx] + Value;
-		Idx = Idx + (Idx & -Idx);
-	}
+ll merge(ll a, ll b) { //for operation of the segment tree
+    return a+b;
+}
+ll segment(int node, int nodeLeft, int nodeRight) { // use when s, a is available and segment tree is about sum
+    if (nodeLeft == nodeRight) {
+        return s[node] = a[nodeLeft];
+    }
+	int mid = (nodeLeft+nodeRight)/2;
+    return s[node] = merge(segment(node * 2, nodeLeft, mid), segment(node * 2 + 1, mid + 1, nodeRight));
+}
+void update(int node, int idx, int nodeLeft, int nodeRight, ll num) {
+    if (idx < nodeLeft || nodeRight < idx) return;
+    if (nodeLeft == nodeRight) {
+        s[node] = num;
+        return;
+    }
+    int mid = (nodeLeft+nodeRight)/2;
+    update(node * 2, idx, nodeLeft, mid, num);
+    update(node * 2 + 1, idx, mid + 1, nodeRight, num);
+
+    s[node] = merge(s[node * 2], s[node * 2 + 1]);
+}
+ll query(int node, int l, int r, int nodeLeft, int nodeRight) { //l and r is the range.
+    if (nodeRight < l || r < nodeLeft) return 0;
+    if (l <= nodeLeft && nodeRight <= r) return s[node];
+	int mid = (nodeLeft+nodeRight)/2;
+    return merge(query(node * 2, l, r, nodeLeft, mid), query(node * 2 + 1, l, r, mid + 1, nodeRight));
+}
+void print(int node, int nodeLeft, int nodeRight) {
+    cout << nodeLeft << " " << nodeRight << " : " << s[node] << endl;
+    if(nodeLeft==nodeRight) return;
+
+    int mid = (nodeLeft + nodeRight) >> 1;
+    print(node*2, nodeLeft, mid);
+    print(node*2+1, mid+1, nodeRight);
+}
+*/
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+/*
+//This is lazy propagation. Beginning starts with segment(..) used in above
+ll lazy[4*MAX] {};
+void propagation(int node, int l, int r) {
+    if (lazy[node]) {
+        s[node] += (r - l + 1) * lazy[node];
+        if (l != r) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+void update(int node, int l, int r, int nodeLeft, int nodeRight, int dif) { //This is for lazy propagation
+    propagation(node, nodeLeft, nodeRight);
+    if (nodeRight < l || r < nodeLeft) return;
+    if (l <= nodeLeft && nodeRight <= r) {
+        s[node] += (nodeRight-nodeLeft + 1) * dif;
+        if (nodeLeft != nodeRight) {
+            lazy[node * 2] += dif;
+            lazy[node * 2 + 1] += dif;
+        }
+        return;
+    }
+    int mid = (nodeLeft+nodeRight)/2;
+    update(node * 2, l, r, nodeLeft, mid, dif);
+    update(node * 2 + 1, l, r, mid + 1, nodeRight, dif);
+    s[node] = merge(s[node * 2], s[node * 2 + 1]);
+}
+ll query(int node, int l, int r, int nodeLeft, int nodeRight) { //s should be vll
+    propagation(node, nodeLeft, nodeRight);
+    if (nodeRight < l || r < nodeLeft) return 0;
+    if (l <= nodeLeft && nodeRight <= r) {
+        return s[node];
+    }
+    int mid = (nodeLeft+nodeRight)/2;
+    return merge(query(node * 2, l, r, nodeLeft, mid), query(node * 2+1, l, r, mid+1, nodeRight));
+}
+
+*/
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+//This is persistent segment tree (PST)
+
+/*
+struct Node {
+    Node *l, *r;
+    ll v;
+
+    Node() {
+        l = r = NULL;
+        v = 0;
+    }  
+};
+
+#define MAX 1000
+//When using MAX, root MAX and arr MAX is DIFFERENT!!!! KEEP IN MIND!!! 
+Node* root[MAX];
+int arr[MAX];
+
+void build(Node *node, int nodeLeft, int nodeRight) {
+    if(nodeLeft == nodeRight) {
+        node->v = arr[nodeLeft];
+        return;
+    }
+    int m = nodeLeft + (nodeRight-nodeLeft)/2;
+
+    node->l = new Node();
+    node->r = new Node();
+   
+    build(node->l, nodeLeft, m);
+    build(node->r, m+1, nodeRight);
+
+    node->v = node->l->v + node->r->v;
+}
+
+//doesn't update origin segment tree but updates new segment tree and connects it into a existing tree
+void update(Node* prev, Node* now, int nodeLeft, int nodeRight, int idx, int value) { 
+    if(nodeLeft == nodeRight) {
+        now->v = value;
+        return;
+    }
+
+    int middle = nodeLeft + (nodeRight-nodeLeft)/2;
+
+    if(idx <= middle) { //update left node
+        now->l = new Node(); now->r = prev->r;
+        update(prev->l, now->l, nodeLeft, middle, idx, value);
+    }
+    else { //update right node
+        now->l = prev->l; now->r = new Node();
+        update(prev->r, now->r, middle+1, nodeRight, idx, value);
+    }
+    now->v = now->l->v + now->r->v;
+}
+
+ll query(Node *node, int nodeLeft, int nodeRight, int l, int r) { //want to know the addition of l~r
+    if(nodeRight < l || r < nodeLeft) return 0;
+    if(l <= nodeLeft && nodeRight <= r) return node->v;
+
+    int middle = nodeLeft + (nodeRight-nodeLeft)/2;
+
+    return query(node->l, nodeLeft, middle, l, r) + query(node->r, middle+1, nodeRight, l, r);
+}
+
+*/
+
+/* could help if above update isn't appliable
+Node* update(Node* now, int nodeLeft, int nodeRight, int idx, int value) {
+    if (nodeRight < idx || idx < nodeLeft) return now;
+    
+    if (nodeLeft == nodeRight) {
+        Node* leaf = new Node();
+        leaf->v = now->v + value;
+        return leaf;
+    }
+
+    int middle = nodeLeft + (nodeRight - nodeLeft) / 2;
+    Node* leaf = new Node();
+    leaf -> l = update(now->l, nodeLeft, middle, idx, value);
+    leaf -> r = update(now->r, middle + 1, nodeRight, idx, value);
+
+
+    leaf->v = leaf->l->v + leaf->r->v;
+    return leaf;
+}
+*/
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+/*
+
+//THIS IS FENWICK_TREE
+//Fenwic_tree starts from index 1. 
+#define MAX 500001
+int arr[MAX];
+int fenwick[MAX];
+
+//update function reflects the change of arr value, not the absolute value
+//if arr value, say arr[3] changes into 3 to 5, then update(3,2) should be used.
+//update(idx, c-arr[idx]); arr[idx] = c;
+void update(int idx, int Value) { //For Making Fenwick Tree, for(int i=1~N) Update(i, arr[i]);
+    while (idx <= N) {
+        fenwick[idx] = fenwick[idx] + Value;
+        idx = idx + (idx & -idx);
+    }
 }
 
 int sum(int idx) { //IF 3~5 sum is required it should be sum(5)-sum(2);
-	int result = 0; //BE CAREFUL ON RANGE (Long Long could be used)
-	while (idx > 0) {
-		result += Fenwick_Tree[idx];
-		idx = idx - (idx & -idx);
-	}
-	return result;
+    int result = 0; //BE CAREFUL ON RANGE (Long Long could be used)
+    while (idx > 0) {
+        result += fenwick[idx];
+        idx = idx - (idx & -idx);
+    }
+    return result;
+}
+
+*/
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+/* 
+
+//two dimensional fenwick tree
+#define MAX 1026
+ll arr[MAX][MAX];
+ll fenwick[MAX][MAX];
+
+void update(int x, int y, ll value) {
+    while(x < N+1) {
+        int tempy = y;
+        while(tempy < N+1) {
+            fenwick[x][tempy] += value;
+            tempy += (tempy & -tempy);
+        }
+
+        x += (x & -x);
+    }
+}
+
+//sum(x,y) means sum of arr[1][1]~arr[x][y]
+ll sum(int x, int y) {
+    ll ret=0;
+    while(x>0) {
+        int tempy = y;
+        while(tempy > 0) {  
+            ret += fenwick[x][tempy];
+            tempy -= (tempy & -tempy);
+        }
+        x -= (x & -x);
+    }
+    return ret;
 }
 */
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
-#define MAX 5001
-vii SCC;
+// THIS IS TARJAN ALGORITHM FOR SCC
+#define MAX 5001 //when used in 2-sat, you have to double node because not only x1~x100 is required but also Nx1~Nx100
+vvi SCC;
 int d[MAX];
 bool finished[MAX];
 vi edge[MAX];
-int id;
+int id, SN; //mark sn[i]
 stack<int> s;
-int dfs(int x) { // THIS IS TARJAN ALGORITHM FOR SCC
-	d[x] = ++id;
-	s.push(x);
-	int parent = d[x];
-	for (auto i : edge[x]) {
-		if (d[i] == 0) {
-			parent = min(parent, dfs(i));
-		}
-		else if (finished[i] == 0) {
-			parent = min(parent, d[i]);
-		}
-	}
-
-	if (parent == d[x]) {
-		vector<int> scc;
-		while (true) {
-			int t = s.top();
-			s.pop();
-			scc.push_back(t);
-			finished[t] = 1;
-			d[t] = x; //to make scc recognizable with d
-			if (t == x) break;
-		}
-		SCC.push_back(scc);
-	}
-	return parent;
+int sn[MAX]; //sn[i] is SCC number to which it belongs to. If sn is big, then it is at the start of DAG. If small, it is at the end of DAG. If one wants to start from the beginning of DAG, start from the largest of sn.
+int SCCnode[MAX] {}; //if SCCbfs is needed...
+int nodeValue[MAX] {}; //if SCCbfs is needed...
+int dfs(int x) {
+    d[x] = ++id; //노드마다 고유한 아이디 부여
+    s.push(x); //스택에 자기 자신을 삽입
+    int parent = d[x];
+    for (auto i : edge[x]) {
+        if (d[i] == 0) { //방문 안 한 이웃
+            parent = min(parent, dfs(i));
+        }
+        else if (finished[i] == 0) { //처리 중인 이웃
+            parent = min(parent, d[i]);
+        }
+    }
+    if (parent == d[x]) {
+        vector<int> scc;
+        while (true) {
+            int t = s.top();
+            s.pop();
+            scc.push_back(t);
+            finished[t] = 1;
+            sn[t] = SN;
+            //d[t] = x; //to make scc recognizable with d
+            //SCCnode[SN] += nodeValue[t];
+            if (t == x) break;
+        }
+        SN++;
+        SCC.push_back(scc); //SCC의 SN번째 그래프랑 대응된다
+    }
+    return parent;
 }
 
+vi SCCedge[MAX]; //index refers to SN. Could be replaced with set if you don't want to overlap
+int inDegree[MAX]; //index refers to SN
+int outDegree[MAX]; //index refers to SN
+void SCCtopology_sort() {
+    for(int i=1;i<=N;i++) { //id starts with 1
+        for(auto next : edge[i]) {
+            if(sn[next]!=sn[i]) {
+                SCCedge[sn[i]].pbk(sn[next]); //SN could be overlapped. Could be solved with set but it is often not needed
+                inDegree[sn[next]]+=1; //If inDegree is 0, then it is the start of the SCC graph. There could be many
+                outDegree[sn[i]]+=1;
+            }
+        }
+    }
+}
+void Solve() {
+    while(true) {
+        cin >> N;
+        if(N==0) return;
+        cin >> M;
+        SCC.clear();
+        rep(i,1,N) {
+            d[i]=0;
+            finished[i]=0;
+            edge[i].clear();
+        }
+        id=SN=0;
+        rep(i,0,N) {
+            sn[i] = 0;
+            SCCedge[i].clear();
+            inDegree[i]=0;
+            outDegree[i]=0;
+        }
 
-/*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*/
-bool visited[MAX];
-bool bfs(int x) {
-	memset(visited, 0, sizeof(visited));
-	queue<int> q;
-	visited[x] = 1;
-	q.push(x);
 
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
+        rep(i,1,M) {
+            int a, b; cin >> a >> b;
+            edge[a].pbk(b);
+        }
+        rep(i,1,N) {
+            if(d[i]==0) dfs(i);
+        }
+        SCCtopology_sort();
+        si answer;
+        for(int i=0;i<SN;i++) {
+            if(outDegree[i]==0) {
+                for(auto j : SCC[i]) answer.insert(j);
+            }
+        }
+        for(auto i : answer) cout << i << " ";
+        cout << endl;
+    }
 
-		for (auto next : edge[cur]) {
-			if (visited[next]) continue;
-
-			if (d[next] != d[cur]) return 1;
-			visited[next] = 1;
-			q.push(next);
-		}
-	}
-	return false;
 }
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	while (true) {
-		memset(d, 0, sizeof(d));
-		memset(finished, 0, sizeof(finished));
-		for (int i = 0; i < MAX; i++) {
-			edge[i].clear();
-		}
-		SCC.clear();
-
-		cin >> N;
-		if (N == 0) break;
-		cin >> K;
-		for (int i = 0; i < K; i++) {
-			int s, e;
-			cin >> s >> e;
-			edge[s].pb(e);
-		}
-
-		for (int i = 1; i <= N; i++) {
-			if (d[i] == 0) dfs(i);
-		}
-
-		set<int> s;
-		for (auto scc : SCC) {
-			int temp = scc.front();
-			if (bfs(temp)) continue;
-			else {
-				for (auto i : scc) {
-					s.insert(i);
-				}
-			}
-		}
-		if (!s.empty()) {
-			for (auto i : s) cout << i << " ";
-		}
-		cout << endl;
-	}
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solve();
 }
