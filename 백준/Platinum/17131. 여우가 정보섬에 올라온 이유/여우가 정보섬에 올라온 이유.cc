289cd805f2d69a46bab1961bee151a3e7103bde9
mbae059 <mbae059@gmail.com>
2023-07-13 09:30:38 +0900

    [Platinum IV] Title: 여우가 정보섬에 올라온 이유, Time: 204 ms, Memory: 35088 KB -BaekjoonHub

---
+++ 백준/Platinum/17131. 여우가 정보섬에 올라온 이유/여우가 정보섬에 올라온 이유.cc
@@ -0,0 +1,196 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 987654321
+#define p_q priority_queue
+#define pbk push_back
+#define rep(i, a, b) for (int i=a; i<=b; i++) 
+#define all(v) (v).begin(), (v).end()
+
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vull = vector<ull>;
+using vvi = vector<vi>;
+using vpii = vector<pii> ;
+using vll = vector<ll>;
+using mii = map<int, int>;
+using si = set<int>;
+using qi = queue<int>;
+using qpii = queue<pii>;
+using tiii = tuple<int, int, int> ; //get<0>(t);
+using tlll = tuple<ll, ll, ll> ; //get<0>(t);
+using vtiii = vector<tiii>;
+using vtlll = vector<tlll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+using spii = set<pii>;
+using qtiii = queue<tiii>;
+int A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+int dy[8] = {1,-1,0,0,1,1,-1,-1};
+int dx[8] = {0,0,1,-1,1,-1,1,-1};
+//for tenary search
+//mid1 = (2*l + r)/3;
+//mid2 = (l + 2*r)/3;
+
+//for finding the intersection of Line(x1,y1,x2,y2) and Line(x3,y3,x4,y4)
+//do not solve with tenary search
+// Px= (x1*y2 - y1*x2)*(x3-x4) - (x1-x2)*(x3*y4 - y3*x4)
+// Py= (x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4)
+
+//int to string : to_string
+//string to int : stoi
+
+//use setw(3) to get nice format for printing out 2-d array
+//ex) cout << setw(3) << "a" << endl;
+
+//to make a sorted vector's element unique, you should do v.erase(unique(v.begin(), v.end()), v.end())
+
+// unordered_map<char,int> dx = {{'D',0},{'L',-1},{'R',1},{'U',0}};
+// unordered_map<char,int> dy = {{'D',1},{'L',0},{'R',0},{'U',-1}}
+
+//diagonal counting. l[y+x], r[y-x+N]
+
+//supports negative modular operation
+ll modular(ll num, ll mod) {
+    num%=mod;
+    return num < 0 ? num+mod : num;
+}
+void print(pii a) {
+    cout << a.first << " " << a.second << endl;
+}
+
+template <typename T>
+void print(const T& a) {
+    cout << a << endl;
+}
+
+void print(const vector<pii>& v) {
+    for(auto p : v) {
+        print(p);
+    }
+    cout << endl;
+}
+template <typename T>
+void print(const vector<T>& v) {
+    for(auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+bool inRange(int y, int x) {
+    return 1<=y && y<=N && 1<=x && x<=M;
+}
+bool inRangeN(int y, int x) {
+    return 1<=y && y<=N && 1<=x && x<=N;
+}
+/*
+vector<int> combination;
+bool visited[1005];
+void dfs(int idx, int cnt) { //implement with dfs(1, 0). N and K must be global variable
+	if(cnt==K) {
+		for(auto i : combination) {
+			cout << i << " ";
+		}
+		cout << endl;
+		return;
+	}
+	
+	for(int i=idx;i<=N;i++) { //idx, N을 잘볼것
+		if(visited[i]==1) continue;
+		
+		visited[i]=1;
+		combination.push_back(i);
+		dfs(i+1, cnt+1); //be careful with i+1, cnt+1
+		visited[i]=0;
+		combination.pop_back();
+	}
+}
+*/
+/* Basic Info
+A-Z is 26 char
+<regex> header file is used for find patterns
+*/
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+
+// This is segment tree
+#define MAX 400001
+ll a[MAX], s[4*MAX];
+
+ll merge(ll a, ll b) { //for operation of the segment tree
+    return a+b;
+}
+ll segment(int node, int nodeLeft, int nodeRight) { // use when s, a is available and segment tree is about sum
+    if (nodeLeft == nodeRight) {
+        return s[node] = a[nodeLeft];
+    }
+	int mid = (nodeLeft+nodeRight)/2;
+    return s[node] = merge(segment(node * 2, nodeLeft, mid), segment(node * 2 + 1, mid + 1, nodeRight));
+}
+void update(int node, int idx, int nodeLeft, int nodeRight, ll dif) {
+    if (idx < nodeLeft || nodeRight < idx) return;
+    if (nodeLeft == nodeRight) {
+        s[node] += dif;
+        return;
+    }
+    int mid = (nodeLeft+nodeRight)/2;
+    update(node * 2, idx, nodeLeft, mid, dif);
+    update(node * 2 + 1, idx, mid + 1, nodeRight, dif);
+
+    s[node] = merge(s[node * 2], s[node * 2 + 1]);
+}
+ll query(int node, int l, int r, int nodeLeft, int nodeRight) { //l and r is the range.
+    if (nodeRight < l || r < nodeLeft) return 0;
+    if (l <= nodeLeft && nodeRight <= r) return s[node];
+	int mid = (nodeLeft+nodeRight)/2;
+    return merge(query(node * 2, l, r, nodeLeft, mid), query(node * 2 + 1, l, r, mid + 1, nodeRight));
+}
+#define MOD 1000000007
+void Solve() {
+    cin >> N;
+    vpll v;
+    rep(i,1,N) {
+        ll x, y; cin >> x >> y;
+        x+=200000;
+        y+=200000;
+        v.pbk({x,y});
+    }
+    M = 400000;
+    sort(all(v), [&](pll a, pll b) {
+        if(a.second==b.second) return a.first < b.first;
+        return a.second > b.second;
+    });
+    vector<vector<ll>> vv;
+    vll temp;
+    for(int i=0;i<v.size();i++) {
+        if(temp.empty() || v.at(i-1).second==v.at(i).second) {
+        }
+        else {
+            vv.pbk(temp);
+            temp.clear();
+        }
+        temp.pbk(v.at(i).first);
+    }
+    vv.pbk(temp);
+    ll sum = 0;
+    for(auto temp : vv) {
+        for(auto i : temp) {
+            sum += (query(1,0,i-1,0,M) * query(1,i+1,M,0,M)) %MOD;
+            sum %= MOD;
+        }
+        for(auto i : temp) {
+            update(1,i,0,M,1);
+        }
+    }
+    cout << sum;
+}
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solve();
+}
